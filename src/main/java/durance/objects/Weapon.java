292ac8293bf1ed0fc69377e5e42443863538c860
Hassan <hassansaleem194@gmail.com>
2022-11-02 13:57:06 +0000

    test(green): passed test to create weapon with no enchantments

--- src/main/java/durance/objects/Weapon.java
+++ src/main/java/durance/objects/Weapon.java
@@ -1,7 +1,47 @@
 package durance.objects;
 
 public class Weapon {
+
+    private int strength;
+    private String name;
+    private int ice;
+    private int fire;
+    private int lifesteal;
+    private int agility;
+
+    public Weapon (String name) {
+        this.name = name;
+        this.ice = 5;
+        this.fire = 5;
+        this.lifesteal = 5;
+        this.agility = 5;
+        this.strength = 5;
+    }
     public void assignEnchant(Enchant enchant) {
         throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getFire() {
+        return fire;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getIce() {
+        return ice;
+    }
+
+    public int getLifesteal() {
+        return lifesteal;
     }
 }
--- src/test/java/durance/objects/WeaponShould.java
+++ src/test/java/durance/objects/WeaponShould.java
@@ -1,12 +1,28 @@
 package durance.objects;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeaponShould {
 
+    private Weapon weapon;
+
     @BeforeEach
     void setUp() {
     }
+
+    @Test public void
+    return_a_weapon_with_no_enchantments() {
+        weapon = new Weapon("dagger of pain");
+        assertThat(weapon.getName()).isEqualTo("dagger of pain");
+        assertThat(weapon.getAgility()).isEqualTo(5);
+        assertThat(weapon.getFire()).isEqualTo(5);
+        assertThat(weapon.getIce()).isEqualTo(5);
+        assertThat(weapon.getStrength()).isEqualTo(5);
+        assertThat(weapon.getLifesteal()).isEqualTo(5);
+    }
+
 }
